70c341320a93fe1cd5cc683acaafd16ae2fb04d1
Josh Fransix <joshiandersonk69@gmail.com>
2021-08-27 14:52:05 +0100

    homePage Mod

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const hamburger = document.querySelector(".hamburger");
 const body = document.querySelector("body");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
+// const lines = document.querySelectorAll(".line");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-open");
+  // lines.classList.toggle("line-open");
   navLinks.classList.toggle("open");
   links.forEach((link) => {
     link.classList.toggle("fade");
