74099370949af9ef3ea3a0fa0db7f1a47bb15263
Josh Fransix <joshiandersonk69@gmail.com>
2021-11-26 00:09:39 +0100

    homePage_Mod

--- main.js
+++ main.js
@@ -2,27 +2,31 @@ const hamburger = document.querySelector(".hamburger");
 
 const body = document.querySelector("body");
 
-const navLinks = document.querySelector(".nav-links");
+const navMenu = document.querySelector(".nav-links");
 
-const links = document.querySelectorAll(".nav-links li");
+const navLinks = document.querySelectorAll(".nav-item");
 
 const header = document.querySelector("#header");
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("hamburger-open");
-  // lines.classList.toggle("line-open");
-  navLinks.classList.toggle("open");
-  links.forEach((link) => {
-    link.classList.toggle("fade");
-  });
-});
+hamburger.addEventListener("click", mobileMenu);
 
-// const cursor = document.querySelector(".cursor");
-// // const allLinks = document.querySelectorAll("a");
-// document.addEventListener("mousemove", (e) => {
-//   cursor.style.left = e.pageX + "px";
-//   cursor.style.top = e.pageY + "px";
-// });
+function mobileMenu() {
+  hamburger.classList.toggle("hamburger-open");
+  navMenu.classList.toggle("open");
+  navLinks.forEach((navLink) => {
+    navLink.classList.toggle("fade");
+  });
+}
+
+navLinks.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("hamburger-open");
+  navMenu.classList.remove("open");
+  navLinks.forEach((navLink) => {
+    navLink.classList.remove("fade");
+  });
+}
 
 window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 0);
